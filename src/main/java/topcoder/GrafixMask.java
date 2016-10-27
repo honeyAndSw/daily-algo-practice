fd5785f1e73320c197a3bab5d43109732ba47e9d
honey.and.sw <honey.and.sw@gmail.com>
2016-10-27 21:54:20 +0900

    src/main, src/test

--- src/main/java/topcoder/GrafixMask.java
+++ src/main/java/topcoder/GrafixMask.java
@@ -1,25 +1,19 @@
 package topcoder;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
 
 /**
  * GrafixMask
  * https://community.topcoder.com/stat?c=problem_statement&pm=2998&rd=5857
- *
- * @author naheon
- * @version 2.0.0
- * @since 2016. 10. 27.
  */
 public class GrafixMask {
 
-	static boolean[][] layer = new boolean[600][400];
+	boolean[][] layer = new boolean[600][400];
 
-	public static void main(String[] args) {
-		int[] output1 = sortedAreas(new String[] {"0 292 399 307"});
-		System.out.println(Arrays.toString(output1));
-	}
-
-	public static int[] sortedAreas(String[] input) {
+	public int[] sortedAreas(String[] input) {
 		for (int i = 0; i < 600; i++) {
 			for (int j = 0; j < 400; j++) {
 				layer[i][j] = false;
@@ -29,7 +23,7 @@ public class GrafixMask {
 		for (String in : input) {
 			int[] rec = parseInput(in);
 			for (int i = rec[1]; i <= rec[3]; i++) {
-				for (int j = rec[0]; j < rec[2]; j++) {
+				for (int j = rec[0]; j <= rec[2]; j++) {
 					layer[i][j] = true;
 				}
 			}
@@ -52,7 +46,7 @@ public class GrafixMask {
 		return result;
 	}
 
-	static class P {
+	class P {
 		int x, y;
 
 		public P(int x, int y) {
@@ -61,7 +55,7 @@ public class GrafixMask {
 		}
 	}
 
-	private static Integer startFill(int x, int y) {
+	private Integer startFill(int x, int y) {
 		int result = 0;
 
 		Stack<P> stack = new Stack<>();
@@ -84,7 +78,7 @@ public class GrafixMask {
 		return result;
 	}
 
-	private static int[] parseInput(String in) {
+	private int[] parseInput(String in) {
 		String[] split = in.split(" ");
 		int[] ret = new int[split.length];
 		for (int i = 0; i < split.length; i++) {
