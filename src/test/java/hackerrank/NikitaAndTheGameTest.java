fedcf014bfbe422707611012433568cdb78eca46
honey.and.sw <honey.and.sw@gmail.com>
2016-11-03 22:28:03 +0900

    Nikita and the Game, test cases

--- src/test/java/hackerrank/NikitaAndTheGameTest.java
+++ src/test/java/hackerrank/NikitaAndTheGameTest.java
@@ -17,4 +17,22 @@ public class NikitaAndTheGameTest {
 		int result = NikitaAndTheGame.score(new int[]{4,1,0,1,1,0,1}, 0, 6);
 		assertEquals(3, result);
 	}
+
+	@Test
+	public void test2() throws Exception {
+		int result = NikitaAndTheGame.score(new int[]{1,0,1}, 0, 2);
+		assertEquals(1, result);
+	}
+
+	@Test
+	public void test3() throws Exception {
+		int result = NikitaAndTheGame.score(new int[]{2,2,2,2}, 0, 3);
+		assertEquals(2, result);
+	}
+
+	@Test
+	public void test4() throws Exception {
+		int result = NikitaAndTheGame.score(new int[]{3,3,3}, 0, 2);
+		assertEquals(0, result);
+	}
 }
