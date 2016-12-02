07d6b0dbf5c1aed4e4901f923fa1f7fdeef958f9
honey.and.sw <honey.and.sw@gmail.com>
2016-12-02 14:22:12 +0900

    Day 1 - Part2. Visited twice

--- src/main/java/adventofcode/NoTimeForATaxicab.java
+++ src/main/java/adventofcode/NoTimeForATaxicab.java
@@ -1,5 +1,7 @@
 package adventofcode;
 
+import java.util.Arrays;
+
 /**
  * NoTimeForATaxicab
  *
@@ -12,9 +14,10 @@ package adventofcode;
  */
 public class NoTimeForATaxicab {
 
+	int MAX_LEN = 300; // Assumed value
+
 	public class Position {
-		int x = 0;
-		int y = 0;
+		int x = 0, y = 0;
 		char dir = 'N';
 
 		public void move(char turn, int b) {
@@ -27,19 +30,90 @@ public class NoTimeForATaxicab {
 					dir = (turn == 'L') ? 'N' : 'S';
 					y = (turn == 'L') ? y + b : y - b;
 					break;
-				case 'S':
-					dir = (turn == 'L') ? 'E' : 'W';
-					x = (turn == 'L') ? x + b : x - b;
+				case 'S': // Reverse of 'N'
+					dir = 'N';
+					move(turn == 'L' ? 'R' : 'L', b);
 					break;
-				case 'W':
-					dir = (turn == 'L') ? 'S' : 'N';
-					y = (turn == 'L') ? y - b : y + b;
+				case 'W': // Reverse of 'E';
+					dir = 'E';
+					move(turn == 'L' ? 'R' : 'L', b);
 					break;
 			}
 		}
 
 		public int blocks() {
 			return Math.abs(x) + Math.abs(y);
+		}
+	}
+
+	public class PositionCheckVisit {
+		int x = 0, y = 0;
+		int tx = 0, ty = 0;
+		char dir = 'N';
+
+		boolean[][] grid = new boolean[MAX_LEN * 2][MAX_LEN * 2];
+		int center = MAX_LEN;
+
+		/**
+		 * Returns true if find a location visited twice.
+		 *
+		 * @param turn
+		 * @param b
+		 * @return
+		 */
+		public boolean move(char turn, int b) {
+			switch (dir) {
+				case 'N':
+					dir = (turn == 'L') ? 'W' : 'E';
+					int nx = (turn == 'L') ? x - b : x + b;
+
+					for (int i = Math.min(nx, x); i <= Math.max(nx, x); i++) {
+						if (grid[y + center][i + center] && (i != x)) {
+							tx = i;
+							ty = y;
+							return true;
+						} else {
+							grid[y + center][i + center] = true;
+						}
+					}
+
+					x = nx;
+					return false;
+				case 'E':
+					dir = (turn == 'L') ? 'N' : 'S';
+					int ny = (turn == 'L') ? y + b : y - b;
+
+					for (int i = Math.min(ny, y); i <= Math.max(ny, y); i++) {
+						if (grid[i + center][x + center] && (i != y)) {
+							tx = x;
+							ty = i;
+							return true;
+						} else {
+							grid[i + center][x + center] = true;
+						}
+					}
+
+					y = ny;
+					return false;
+				case 'S': // Reverse of 'N'
+					dir = 'N';
+					return move(turn == 'L' ? 'R' : 'L', b);
+				case 'W': // Reverse of 'E';
+					dir = 'E';
+					return move(turn == 'L' ? 'R' : 'L', b);
+				default:
+					return false;
+			}
+		}
+
+		public int blocks() {
+			return Math.abs(tx) + Math.abs(ty);
+		}
+
+		public void grid() {
+			for (int i = 0; i < grid.length; i++) {
+				System.out.println(Arrays.toString(grid[i]));
+			}
 		}
 	}
 
@@ -50,6 +124,21 @@ public class NoTimeForATaxicab {
 			char[] d = i.substring(0, 1).toCharArray();
 			int b = Integer.parseInt(i.substring(1, i.length()));
 			pos.move(d[0], b);
+		}
+		return pos.blocks();
+	}
+
+	public int blocksToVisitTwice(String[] input) {
+		PositionCheckVisit pos = new PositionCheckVisit();
+
+		for (String i : input) {
+			char[] d = i.substring(0, 1).toCharArray();
+			int b = Integer.parseInt(i.substring(1, i.length()));
+
+			boolean visitTwice = pos.move(d[0], b);
+			if (visitTwice) {
+				return pos.blocks();
+			}
 		}
 
 		return pos.blocks();
--- src/test/java/adventofcode/NoTimeForATexicabTest.java
+++ src/test/java/adventofcode/NoTimeForATexicabTest.java
@@ -2,6 +2,8 @@ package adventofcode;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,6 +16,7 @@ import org.junit.Test;
  */
 public class NoTimeForATexicabTest {
 	NoTimeForATaxicab cab;
+	String[] input = new String[] {"L5","R1","L5","L1","R5","R1","R1","L4","L1","L3","R2","R4","L4","L1","L1","R2","R4","R3","L1","R4","L4","L5","L4","R4","L5","R1","R5","L2","R1","R3","L2","L4","L4","R1","L192","R5","R1","R4","L5","L4","R5","L1","L1","R48","R5","R5","L2","R4","R4","R1","R3","L1","L4","L5","R1","L4","L2","L5","R5","L2","R74","R4","L1","R188","R5","L4","L2","R5","R2","L4","R4","R3","R3","R2","R1","L3","L2","L5","L5","L2","L1","R1","R5","R4","L3","R5","L1","L3","R4","L1","L3","L2","R1","R3","R2","R5","L3","L1","L1","R5","L4","L5","R5","R2","L5","R2","L1","L5","L3","L5","L5","L1","R1","L4","L3","L1","R2","R5","L1","L3","R4","R5","L4","L1","R5","L1","R5","R5","R5","R2","R1","R2","L5","L5","L5","R4","L5","L4","L4","R5","L2","R1","R5","L1","L5","R4","L3","R4","L2","R3","R3","R3","L2","L2","L2","L1","L4","R3","L4","L2","R2","R5","L1","R2"};
 
 	@Before
 	public void setUp() throws Exception {
@@ -29,23 +32,23 @@ public class NoTimeForATexicabTest {
 
 	@Test
 	public void test2() throws Exception {
-		String[] input = new String[]{"L5", "R1", "L5", "L1", "R5", "R1", "R1", "L4", "L1", "L3", "R2", "R4", "L4", "L1", "L1",
-			"R2", "R4", "R3", "L1", "R4", "L4", "L5", "L4", "R4", "L5", "R1", "R5", "L2", "R1", "R3", "L2", "L4", "L4", "R1",
-			"L192", "R5", "R1", "R4", "L5", "L4", "R5", "L1", "L1", "R48", "R5", "R5", "L2", "R4", "R4", "R1", "R3", "L1",
-			"L4", "L5", "R1", "L4", "L2", "L5", "R5", "L2", "R74", "R4", "L1", "R188", "R5", "L4", "L2", "R5", "R2", "L4",
-			"R4", "R3", "R3", "R2", "R1", "L3", "L2", "L5", "L5", "L2", "L1", "R1", "R5", "R4", "L3", "R5", "L1", "L3",
-			"R4", "L1", "L3", "L2", "R1", "R3", "R2", "R5", "L3", "L1", "L1", "R5", "L4", "L5", "R5", "R2", "L5", "R2",
-			"L1", "L5", "L3", "L5", "L5", "L1", "R1", "L4", "L3", "L1", "R2", "R5", "L1", "L3", "R4", "R5", "L4", "L1",
-			"R5", "L1", "R5", "R5", "R5", "R2", "R1", "R2", "L5", "L5", "L5", "R4", "L5", "L4", "L4", "R5", "L2", "R1",
-			"R5", "L1", "L5", "R4", "L3", "R4", "L2", "R3", "R3", "R3", "L2", "L2", "L2", "L1", "L4", "R3", "L4", "L2",
-			"R2", "R5", "L1", "R2"};
-
-		System.out.println(cab.blocksToHq(input));
+		assertEquals(226, cab.blocksToHq(input));
 	}
 
 	@Test
 	public void test3() throws Exception {
 		assertEquals(0, cab.blocksToHq(new String[]{"R5", "R5", "R5", "R5"}));
 		assertEquals(192, cab.blocksToHq(new String[]{"L192"}));
+	}
+
+	@Test
+	public void test4() throws Exception {
+		assertEquals(4, cab.blocksToVisitTwice(new String[]{"R8", "R4", "R4", "R8"}));
+	}
+
+	@Test
+	public void test5() throws Exception {
+		System.out.println(cab.blocksToVisitTwice(input));
+
 	}
 }
