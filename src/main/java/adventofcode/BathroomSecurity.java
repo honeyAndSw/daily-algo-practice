ce8f94a0bfa04611e8b9384dd63c532aa15dce91
honey.and.sw <honey.and.sw@gmail.com>
2016-12-04 14:08:17 +0900

    Day 2 - Part2. Alphanumeric keypad

--- src/main/java/adventofcode/BathroomSecurity.java
+++ src/main/java/adventofcode/BathroomSecurity.java
@@ -16,7 +16,7 @@ public class BathroomSecurity {
 		this.instructions = instructions;
 	}
 
-	public String password() {
+	public String numericPassword() {
 		int prev = 4; // means starting from '5'
 		StringBuilder pw = new StringBuilder();
 
@@ -43,5 +43,41 @@ public class BathroomSecurity {
 		}
 
 		return pw.toString();
+	}
+
+	public String alphaNumericPassword() {
+		char[] keypad = new char[]{
+			'X', 'X', '1', 'X', 'X',
+			'X', '2', '3', '4', 'X',
+			'5', '6', '7', '8', '9',
+			'X', 'A', 'B', 'C', 'X',
+			'X', 'X', 'D', 'X', 'X'};
+
+		int prev = 10; // means starting from '5'
+		StringBuffer sb = new StringBuffer();
+
+		for (String inst : instructions) {
+			for (char ch : inst.toCharArray()) {
+				switch (ch) {
+					case 'U':
+						if (prev - 5 >= 0 && keypad[prev - 5] != 'X') prev -= 5;
+						break;
+					case 'D':
+						if (prev + 5 <= 24 && keypad[prev + 5] != 'X') prev += 5;
+						break;
+					case 'L':
+						int min = (prev / 5) * 5;
+						if (prev - 1 >= min && prev - 1 <= min + 4 && keypad[prev - 1] != 'X') prev -= 1;
+						break;
+					case 'R':
+						min = (prev / 5) * 5;
+						if (prev + 1 >= min && prev + 1 <= min + 4 && keypad[prev + 1] != 'X') prev += 1;
+						break;
+				}
+			}
+			sb.append(keypad[prev]);
+		}
+
+		return sb.toString();
 	}
 }
